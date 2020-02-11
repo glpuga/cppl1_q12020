a7079b17b2223d3c05df075faaaf6c262197db97
LucasJSch <lscheink@creativa77.com.ar>
2020-02-11 00:47:40 +0000

    Created Vector3 .h and .cc initial files.

---
+++ course/include/vector3.h
@@ -0,0 +1,79 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include <cstdint>
+#include <iostream>
+#include <istream>
+#include <ostream>
+
+namespace ekumen {
+namespace math {
+
+class Vector3{
+    public:
+        // Constructors
+        Vector3() : x_(0), y_(0), z_(0) {};
+        Vector3(double x, double y = 0, double z = 0) : x_(x), y_(y), z_(z) {};
+        Vector3(const Vector3& v) : x_(v.x()), y_(v.y()), z_(v.z()) {} ;
+
+        // Destructor
+        ~Vector3() {}; // is this necessary?
+
+        // Operators
+        double& operator [] (const int);
+        double& operator [] (const int) const;
+        Vector3 operator + (const Vector3&) const;
+        Vector3 operator - (const Vector3&) const;
+        Vector3 operator * (const Vector3&) const;
+        Vector3 operator / (const Vector3&) const;
+        Vector3 operator = (const Vector3&);
+        Vector3& operator += (const Vector3&);
+        Vector3& operator -= (const Vector3&);
+        Vector3& operator *= (const Vector3&);
+        Vector3& operator /= (const Vector3&);
+        bool operator == (const Vector3&) const;
+        bool operator != (const Vector3&) const;
+        //std::stringstream& operator << (std::stringstream&) const;
+
+        friend std::ostream& operator << (std::ostream& os, const Vector3& v)
+        {
+            os << std::string("(x: ") << v.x() << ", y: " << v.y() << ", z: " << v.z() << ")";
+            return os;
+        }
+
+        // Getters
+        double x() const;
+        double y() const;
+        double z() const;
+
+        // Setters
+        double& x();
+        double& y();
+        double& z();
+
+        // Computations
+        double dot(const Vector3&) const;
+        double norm() const;
+        Vector3 cross(const Vector3&) const;
+
+        // Class constants
+
+        static const Vector3 kUnitX;
+        static const Vector3 kUnitY;
+        static const Vector3 kUnitZ;
+        static const Vector3 kZero;
+        static const int MAX_INDEX = 2;
+
+    private:
+        // Attributes
+        double x_;
+        double y_;
+        double z_;
+        // Vector data in array-form, for more efficient implementation of [] operator
+        double* data_[3] = {&x_, &y_, &z_};
+};
+
+}  // namespace math
+}  // namespace ekumen
+
+#endif  // VECTOR3_H
---
+++ course/src/vector3.cc
@@ -0,0 +1,134 @@
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include "vector3.h"
+
+namespace ekumen {
+namespace math {
+
+double& Vector3::operator [] (const int index) const {
+    if(index > Vector3::MAX_INDEX) {
+        //throw error
+    }
+    return *(data_[index]);
+}
+
+double& Vector3::operator [] (const int index) {
+    if(index > Vector3::MAX_INDEX) {
+        //throw error
+    }
+    return *(data_[index]);
+}
+
+Vector3 Vector3::operator + (const Vector3& v) const {
+    return Vector3(x_ + v.x_, y_ + v.y_, z_ + v.z_);
+}
+
+Vector3 Vector3::operator - (const Vector3& v) const {
+    return Vector3(x_ - v.x_, y_ - v.y_, z_ - v.z_);
+}
+
+Vector3 Vector3::operator * (const Vector3& v) const {
+    return Vector3(x_ * v.x_, y_ * v.y_, z_ * v.z_);
+}
+
+Vector3 Vector3::operator / (const Vector3&v) const {
+    return Vector3(x_ / v.x_, y_ / v.y_, z_ / v.z_);
+}
+
+Vector3 Vector3::operator = (const Vector3& v) {
+    x_ = v.x();
+    y_ = v.y();
+    z_ = v.z();
+    return Vector3(v); 
+}
+
+Vector3& ekumen::math::Vector3::operator += (const Vector3& v) { 
+    x_ += v.x_;
+    y_ += v.y_;
+    z_ += v.z_;
+    return *this;
+}
+
+Vector3& ekumen::math::Vector3::operator -= (const Vector3& v) {
+    x_ -= v.x_;
+    y_ -= v.y_;
+    z_ -= v.z_;
+    return *this; 
+}
+
+Vector3& ekumen::math::Vector3::operator *= (const Vector3& v) {
+    x_ *= v.x_;
+    y_ *= v.y_;
+    z_ *= v.z_;
+    return *this; 
+}
+
+Vector3& ekumen::math::Vector3::operator /= (const Vector3& v) {
+    x_ /= v.x_;
+    y_ /= v.y_;
+    z_ /= v.z_;
+    return *this; 
+}
+
+bool ekumen::math::Vector3::operator == (const Vector3& v) const {
+    return (x_ == v.x_) && (y_ == v.y_) && (z_ == v.z_);
+}
+
+bool ekumen::math::Vector3::operator != (const Vector3& v) const {
+    // introduce tolerance
+    return (x_ != v.x_) || (y_ != v.y_) || (z_ != v.z_);
+}
+
+// Getters
+double Vector3::x() const {
+    return x_;
+}
+
+double Vector3::y() const {
+    return y_;
+}
+
+double Vector3::z() const {
+    return z_;
+}
+
+// Setters
+double& Vector3::x() {
+    return x_;
+}
+
+double& Vector3::y() {
+    return y_;
+}
+
+double& Vector3::z() {
+    return z_;
+}
+
+// Computations
+double Vector3::dot(const Vector3& v) const {
+    return (x_ * v.x_) + (y_ * v.y_) + (z_ * v.z_);
+}
+
+double Vector3::norm() const {
+    return sqrt( (x_ * x_) + (y_ * y_) + (z_* z_) );
+}
+
+Vector3 Vector3::cross(const Vector3& v) const {
+    return Vector3(
+    y_ * v.z_ - z_ * v.y_, 
+    z_ * v.x_ - x_ * v.z_,
+    x_ * v.y_ - y_ * v.x_);
+}
+
+// Class constants
+const Vector3 Vector3::kUnitX = Vector3(1,0,0);
+const Vector3 Vector3::kUnitY = Vector3(0,1,0);
+const Vector3 Vector3::kUnitZ = Vector3(0,0,1);
+const Vector3 Vector3::kZero = Vector3(0,0,0);
+
+}  // namespace math
+}  // namespace ekumen
