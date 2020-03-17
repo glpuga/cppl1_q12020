c978edab7e98b09a1002feeccf8a982dd6676993
lobotuerk <jtlorente@ekumenlabs.com>
2020-03-17 16:18:48 -0300

    fix bug
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

--- course/src/main.cc
+++ course/src/main.cc
@@ -4,8 +4,5 @@
 int main(int argc, char **argv) {
   (void) argc;
   (void) argv;
-  // const ekumen::math::Isometry t5{ekumen::math::Isometry::RotateAround(ekumen::math::Vector3::kUnitZ, M_PI / 8.)};
-  //
-  // std::cout << t5 << std::endl;
   return 0;
 }
